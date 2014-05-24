d337cea8d4dde305902078a1cf48b726ae439a82
Sergey Romanov <sromanov-dev@yandex.ru>
2014-05-24 08:23:28 -0700

    fix syntax mappings

--- lib/App/Nopaste/Service/dpaste.pm
+++ lib/App/Nopaste/Service/dpaste.pm
@@ -3,40 +3,31 @@ package App::Nopaste::Service::dpaste;
 use strict;
 use warnings;
 
+use JSON ();
+
 our $VERSION = '0.02';
+
+our $SYNTAX_CHOICES_URL = 'http://dpaste.com/api/v2/syntax-choices/';
 
 use base 'App::Nopaste::Service';
 
 sub uri { 'http://dpaste.com/' }
 
-my $languages = {
-    apacheconf => 'Apache',
-    bash       => 'Bash',
-    css        => 'Css',
-    diff       => 'Diff',
-    django     => 'DjangoTemplate',
-    erb        => 'Rhtml',
-    haskell    => 'Haskell',
-    js         => 'JScript',
-    pycon      => 'PythonConsole',
-    python     => 'Python',
-    ruby       => 'Ruby',
-    sql        => 'Sql',
-    xml        => 'Xml',
-    text       => q(),
-};
-
 sub fill_form {
-    my( $self, $mech, %args ) = @_;
+    my ($self, $mech, %args) = @_;
 
-    my $lang = $args{lang} || 'text';
+    my $syntax_map = $self->get_syntax_map($mech);
+
+    my $syntax = $syntax_map->{$args{lang}}
+      ? $syntax_map->{$args{lang}}
+      : $syntax_map->{text};
 
     $mech->submit_form(
         form_name => 'pasteform',
         fields    => {
-            content  => $args{text},
-            hold     => 'on',
-            language => $languages->{$lang},
+            content     => $args{text},
+            syntax      => $syntax,
+            expiry_days => 1,
             ( $args{nick}
                 ? (poster => $args{nick})
                 : ()
@@ -49,10 +40,23 @@ sub fill_form {
     );
 }
 
-sub return {
-    my( $self, $mech ) = @_;
+sub get_syntax_map {
+    my ($self, $mech) = @_;
 
-    my $link = $mech->uri;
+    my $res = $mech->get($SYNTAX_CHOICES_URL);
+
+    die "Unable to fetch $SYNTAX_CHOICES_URL: @{[$res->status_line()]}"
+        unless $res->is_success();
+
+    $mech->back();
+
+    return JSON->new->decode($res->content());
+}
+
+sub return {
+    my ($self, $mech) = @_;
+
+    my $link = $mech->uri();
 
     return (1, $link);
 }
@@ -72,7 +76,14 @@ App::Nopaste::Service::dpaste - L<App::Nopaste> interface to L<http://dpaste.com
 
     nopaste -s dpaste -l haskell foo_file.hs
 
+=head1 DESCRIPTION
+
+This is an L<App::Nopaste> back-end for L<http://dpaste.com> pastebin.
+All pastes will be expired in 1 day.
+
 =head1 SEE ALSO
+
+L<http://dpaste.com/api/v2/syntax-choices/> - available syntax mappings
 
 L<App::Nopaste::Command> - command-line utility for L<App::Nopaste>
 
